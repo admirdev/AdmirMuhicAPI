9e87eaea99b46376e4602c2096854b8d1e69967e
Admir <admir.muhic@outlook.com>
2026-03-01 00:08:38 +0100

    Add project files and deployment workflow

---
+++ AdmirMuhicAPI/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdmirMuhicAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
